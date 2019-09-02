0a77229b100de5c1f550e2bbfa82f52187e1e8ba
moyosore <moyosore@gmail.com>
2019-09-02 17:09:40 +0100

    second commit

--- script.js
+++ script.js
@@ -1,18 +1,85 @@
-var exp ='', number, decimal, equal, operator, allowSR;
-var textview = document.forms['myForm']['textview'];
-
-function  insertNum(num){
-    if (equal) {
-        exp = num;
-        textview.value = exp;
-        number = true;
-        equal = false;
+function getnum(num){
+    let input_num = document.getElementById("textview");
+    switch(num) {
+        case 1:
+            input_num.value +="1";
+            break;
+        case 2:
+            input_num.value +="2";
+            break;
+        case 3:
+            input_num.value +="3";
+            break;
+        case 4:
+            input_num.value +="4";
+            break;
+        case 5:
+            input_num.value +="5";
+            break;
+        case 6:
+            input_num.value +="6";
+            break;
+        case 7:
+            input_num.value +="7";
+            break;
+        case 8:
+            input_num.value +="8";
+            break;
+        case 9:
+            input_num.value +="9";
+            break;
+        case 0:
+            input_num.value +="0";
+            break;
     }
-    else {
-        exp = textview.value + num;
-        textview.value = exp;
-        number = true;
-    }
-    if (operator) decimal = false;
-    //SR('a');
 }
+
+//clear screen function
+function clearnum(){
+    document.getElementById("textview").value ="";
+}
+
+//operators
+function operator(sign){
+    let input_num =document.getElementById("textview")
+    switch(sign){
+        case "+":
+                input_num.value +="+";
+                break;
+            case "-":
+                input_num.value +="-";
+                break;
+            case "*":
+                input_num.value +="*";
+                break;
+            case "/":
+                input_num.value +="/";
+                break; 
+                  case "%":
+                input_num.value +="%";
+                break;
+            case ".":
+                input_num.value +=".";
+                break;
+          
+    }
+}
+
+//backspace
+    function backspace() {
+        let input_num = document.getElementById("textview");
+        let bspace = input_num.value;
+
+        if (bspace.length > 0) {
+            bspace = bspace.substring(0, bspace.length-1);
+            input_num.value = bspace;
+        }
+        
+    }
+
+    //EqualTo Function
+    function compute() {
+        let input_num = document.getElementById("textarea");
+        ans = Math.floor(+eval(input_num.value));
+        document.getElementById('textview').value = "=" + ans;
+    }
